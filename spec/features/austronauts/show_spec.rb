39e5e722c8a1a797f8e55a9d4c47e7b9762057c0
David Gomez <davdg25@gmail.com>
2019-10-11 10:15:52 -0600

    think i fixed all astronaut misspellings

--- spec/features/austronauts/show_spec.rb
+++ spec/features/austronauts/show_spec.rb
@@ -1,24 +1,24 @@
-# require 'rails_helper'
-#
-# RSpec.describe 'Austronaut index page', type: :feature do
-#   describe 'as a user ' do
-#     before(:each) do
-#       @austronaut_1 = Austronaut.create(name: 'David', age: '26', job: 'Austronaut 1')
-#       @austronaut_2 = Austronaut.create(name: 'Scott', age: '26', job: 'Austronaut 2')
-#       @austronaut_3 = Austronaut.create(name: 'Ryan', age: '32', job: 'Austronaut 3')
-#       @austronaut_4 = Austronaut.create(name: 'Ryan', age: '26', job: 'Austronaut 4')
-#     end
-#
-#     it 'I can see a list of all austronauts and their name, age, and job' do
-#       visit '/austronauts'
-#
-#       expect(page).to have_content(@austronaut_1.name)
-#       expect(page).to have_content(@austronaut_1.age)
-#       expect(page).to have_content(@austronaut_1.job)
-#       expect(page).to have_content(@austronaut_2.name)
-#       expect(page).to have_content(@austronaut_2.age)
-#       expect(page).to have_content(@austronaut_2.job)
-#     end
-#
-#   end
-# end
+require 'rails_helper'
+
+RSpec.describe 'Austronaut index page', type: :feature do
+  describe 'as a user ' do
+    before(:each) do
+      @astronaut_1 = Astronaut.create(name: 'David', age: '26', job: 'Austronaut 1')
+      @astronaut_2 = Astronaut.create(name: 'Scott', age: '26', job: 'Austronaut 2')
+      @astronaut_3 = Astronaut.create(name: 'Ryan', age: '32', job: 'Austronaut 3')
+      @astronaut_4 = Astronaut.create(name: 'Ryan', age: '26', job: 'Austronaut 4')
+    end
+
+    it 'I can see a list of all astronauts and their name, age, and job' do
+      visit '/astronauts'
+
+      expect(page).to have_content(@astronaut_1.name)
+      expect(page).to have_content(@astronaut_1.age)
+      expect(page).to have_content(@astronaut_1.job)
+      expect(page).to have_content(@astronaut_2.name)
+      expect(page).to have_content(@astronaut_2.age)
+      expect(page).to have_content(@astronaut_2.job)
+    end
+
+  end
+end
